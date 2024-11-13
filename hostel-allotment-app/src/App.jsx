17e809ed0a0472c99df1367bcd6ce2cdb86c4a97
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2024-11-13 18:05:36 +0530

    Clear app.js

--- hostel-allotment-app/src/App.jsx
+++ hostel-allotment-app/src/App.jsx
@@ -2,14 +2,8 @@ import { useState } from 'react'
 import LoginButton from './components/button'
 
 export default function App() {
-  return (
-<<<<<<< HEAD
-      <LoginButton/>
-=======
-    <h1 className="text-3xl font-bold underline">
-      App
-      suman
-    </h1>
->>>>>>> 270dfdd5c1454bf69c5cdaadca2aacf6bb6a839d
+  return(
+    <> 
+    </>
   )
 }

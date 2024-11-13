ab12b0bf59b3b6041176e3b188475f4cbb3d0811
mani-chauhan2004 <chauhan.mani1070@gmail.com>
2024-11-13 12:33:51 +0530

    Add button component definition

--- hostel-allotment-app/src/App.jsx
+++ hostel-allotment-app/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LoginButton from './components/button'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      App
-    </h1>
+      <LoginButton/>
   )
 }

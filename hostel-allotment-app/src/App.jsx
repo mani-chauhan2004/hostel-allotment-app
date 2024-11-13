c1ea9b509be03e8f5ee505c9c089e5e27d92a202
Suman Kumar <suman.k9709@gmail.com>
2024-11-14 00:00:08 +0530

    remove some comment

--- hostel-allotment-app/src/App.jsx
+++ hostel-allotment-app/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import LoginPage from './components/LoginPage';
 
-// import RegistrationPage from './components/RegistrationPage';
 import LoginPage from './components/LoginPage';
 import RegistrationPage from './components/RegisterPage';
-// import HomePage from './components/HomePage';
+
 
 export default function App() {
   return (

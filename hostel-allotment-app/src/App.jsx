ee55fa7a92490aa2a2c4c495262bc6778373c728
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2024-11-13 23:14:27 +0530

    new commit

--- hostel-allotment-app/src/App.jsx
+++ hostel-allotment-app/src/App.jsx
@@ -8,13 +8,10 @@ import LoginPage from './components/LoginPage'
 
 export default function App() {
   return (
-<<<<<<< HEAD
-      <LoginButton/>
-=======
-    <h1 className="text-3xl font-bold underline">
-      App
-      suman
-    </h1>
->>>>>>> 270dfdd5c1454bf69c5cdaadca2aacf6bb6a839d
+<>
+<LoginButton/>
+</>
+
+
   )
 }
